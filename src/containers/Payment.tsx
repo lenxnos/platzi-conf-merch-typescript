e84a32f2c36645b26544cb505de9a458588bcd8e
Nelson <nelson.soto.c05@gmail.com>
2022-08-09 00:55:11 -0400

    feat: integrate button paypal to payment

--- src/containers/Payment.tsx
+++ src/containers/Payment.tsx
@@ -1,12 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../context';
 import '../styles/components/Payment.css';
 
+import { handleSumCartTotal } from '../utils';
+import PaypalButton from '../components/PaypalButton';
+
 const Payment: FC = () => {
+  const {
+    state: { cart, buyer },
+    addNewOrder,
+  } = useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  const totalToPay = handleSumCartTotal(cart);
+
+  const handlePaymentSuccess = (data: any) => {
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      navigate('/checkout/success');
+    }
+  };
+
+  const handlePaymentError = (error: any) => {
+    console.log(error);
+  };
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen de la orden:</h3>
-        <div className="Payment-button">Botón de pago con Paypal</div>
+        {cart.map((product) => (
+          <div className="Payment-item" key={product.title}>
+            <h4>{product.title}</h4>
+            <span>$ {product.price}</span>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PaypalButton
+            amount={totalToPay.toFixed(2)}
+            onPaymentSuccess={handlePaymentSuccess}
+            onPaymentError={handlePaymentError}
+          />
+        </div>
       </div>
       {/** TODO - Add sidebar */}
       <div className="Payment-sidebar" />
