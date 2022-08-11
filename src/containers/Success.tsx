8c66f3c13570f750949f1e212d31ac47a76bff32
Nelson <nelson.soto.c05@gmail.com>
2022-08-11 00:32:07 -0400

    feat: integreate map and fetch location

--- src/containers/Success.tsx
+++ src/containers/Success.tsx
@@ -1,16 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import Map from '../components/Map';
+import { AppContext } from '../context';
+import { useLocationAddress } from '../hooks';
 
 import '../styles/components/Success.css';
 
 const Success: FC = () => {
+  const {
+    state: { buyer: buyers },
+  } = useContext(AppContext);
+  const [buyer] = buyers;
+
+  const address = useLocationAddress(buyer?.address || '');
+
+  const location = [address.lat, address.lng] as [number, number];
+
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>Gracias por tu compra</h2>
+        <h2>{`${buyer?.name} Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 días a tu dirección:</span>
         <div className="Success-map">
-          {/** TODO - Add map */}
-          Google Maps
+          <Map location={location} />
         </div>
       </div>
     </div>
